a933fe8303044b6953a64ade38f1f5eca6d0babc
Filipe Muniz <filipe@meira.net>
2026-08-11 15:39:56 -0300

    V1.0: worker da cloudflare adicionado

---
+++ worker.js
@@ -0,0 +1,11 @@
+import html from './index.html'
+
+export default {
+  async fetch(request, env) {
+    return new Response(html, {
+      headers: {
+        'Content-Type': 'text/html;charset=UTF-8',
+  },
+  });
+  }
+};
